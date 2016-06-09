c9063763ed987b264d24d3cb19afbe082e1efe8e
VWIJECH <chathuranga.wijetunga@pearson.com>
2016-06-09 18:44:47 +0530

    Test

--- src/main/shell_scripts/compounds.sh
+++ src/main/shell_scripts/compounds.sh
@@ -1,5 +1,9 @@
 git clone https://github.com/Pearson-Higher-Ed/compounds.git
-cd compounds/
+cd compounds
+node --version
+npm --version
+npm install
+npm run build
 git checkout laxapana-GCAPI-1188--Calendar
 npm install
 npm start
